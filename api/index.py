b67a3209061e021a8ff05d8f1f811a0c5db088ca
Aman Kumar <aman9995199@gmail.com>
2026-06-10 04:36:34 -0400

    Auto-create superuser admin on Vercel cold start

--- api/index.py
+++ api/index.py
@@ -8,6 +8,11 @@ django.setup()
 from django.core.management import call_command
 call_command('migrate', '--noinput')
 
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser(username='admin', password='l', first_name='Super Admin')
+    print('Superuser "admin" created')
+
 from ad_generator.wsgi import application
 
 app = application
