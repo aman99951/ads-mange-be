c75cc6edaae0c792e6bd94b8d84188c8664edeef
Aman Kumar <aman9995199@gmail.com>
2026-07-13 03:49:00 -0400

    add 3

--- ad_generator/settings.py
+++ ad_generator/settings.py
@@ -93,7 +93,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = '/tmp/staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
